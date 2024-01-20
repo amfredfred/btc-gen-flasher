6083cc8dae05337412df844aec24e9420ee694af
Dev Fred <manfredtochukwu362@gmail.com>
2024-01-20 16:26:53 +0100

    --remade

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -2,7 +2,7 @@ import AboutSection from "../../Sections/About";
 import BannerSection from "../../Sections/Banner";
 import DemoSection from "../../Sections/Demos";
 import FeaturesSection from "../../Sections/Features";
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import DemoGallerySection from "../../Sections/Gallery";
@@ -11,19 +11,21 @@ import ProductSection from "../../Sections/Product";
 
 export default function Landing() {
     return (
-        <Swiper
-            slidesPerView={1}
-            direction="vertical"
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper, "hjkl")}
-            style={{ height: '100dvh', maxWidth:'100dvw' }}
-            mousewheel={{ invert: true }} >
-            <SwiperSlide> <BannerSection /></SwiperSlide>
-            <SwiperSlide> <AboutSection /></SwiperSlide>
-            <SwiperSlide> <DemoSection /></SwiperSlide>
-            <SwiperSlide> <FeaturesSection /></SwiperSlide>
-            <SwiperSlide> <DemoGallerySection /></SwiperSlide>
-            <SwiperSlide> <ProductSection /></SwiperSlide>
-        </Swiper>
+        // <Swiper
+        //     slidesPerView={1}
+        //     direction="vertical"
+        //     onSlideChange={() => console.log('slide change')}
+        //     onSwiper={(swiper) => console.log(swiper, "hjkl")}
+        //     style={{ height: '100dvh', maxWidth:'100dvw' }}
+        //     mousewheel={{ invert: true }} >
+        <>
+            {/*<SwiperSlide>*/} <BannerSection />{/*</SwiperSlide>*/}
+            {/*<SwiperSlide>*/} <AboutSection />{/*</SwiperSlide>*/}
+            {/*<SwiperSlide>*/} <DemoSection />{/*</SwiperSlide>*/}
+            {/*<SwiperSlide>*/} <FeaturesSection />{/*</SwiperSlide>*/}
+            {/*<SwiperSlide>*/} <DemoGallerySection />{/*</SwiperSlide>*/}
+            {/*<SwiperSlide>*/} <ProductSection />{/*</SwiperSlide>*/}
+            {/* </Swiper> */}
+        </>
     )
 }
